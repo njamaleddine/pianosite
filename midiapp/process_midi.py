e9592d7421d758371838429d82695f583778c825
Nabil Jamaleddine <nabilj123@gmail.com>
2015-03-08 23:05:39 -0400

    Audio slicing with pydub

--- midiapp/process_midi.py
+++ midiapp/process_midi.py
@@ -44,29 +44,19 @@ def sample_midi(filename, total_length=(60 * 1000000) * 30):
 # midi.write_midifile("mary2.mid", sampled_midi)
 
 
-def is_fluidsynth_installed():
-    """ Check to make sure fluidsynth exists in the PATH """
-    for path in os.environ['PATH'].split(os.pathsep):
-        f = os.path.join(path, 'fluidsynth')
-        if os.path.exists(f) and os.access(f, os.X_OK):
-            return True
-
-    return False
-
-
-def convert_to_audio(midi_file, soundfont='fluidr3_gm2-2.sf2', output_path='../pianosite/public/media/audio/', output_types=['oga']):
+def convert_to_audio(midi_filename, soundfont='fluidr3_gm2-2.sf2', output_path='../pianosite/public/media/audio/', output_types=['oga']):
     """
     Convert a single midi file to an audio given a list of output_types
 
     paramters:
-    `midi_file`: the filename of the midi file to convert
+    `midi_filename`: the filename of the midi file to convert
     `soundfont`: the soundfont filename used in converting the midi to audio
     `output_types`: accepted file output types (wav, oga, ogg, etc.)
     """
     if not soundfont:
         return "You need to download a soundfont file to convert the midi into audio!"
     try:
-        filename = midi_file.split(".")[0]
+        filename = midi_filename.split(".")[0]
 
         for output_type in output_types:
             output_filename = u"{}{}.{}".format(
@@ -75,29 +65,45 @@ def convert_to_audio(midi_file, soundfont='fluidr3_gm2-2.sf2', output_path='../p
             subprocess.call(
                 [
                     'fluidsynth', '-T', output_type, '-F', output_filename,
-                    '-ni', soundfont, midi_file
+                    '-ni', soundfont, midi_filename
                 ]
             )
     except:
         raise "Incorrect File type or file has no name!"
 
 
-def slice_audio(audio_file, file_type='oga', length=(30 * 1000)):
+def slice_audio(audio_file_name, length=(30 * 1000)):
     """
     Using pydub to slice 30 seconds from the middle of the song
+
+    Output ogg and mp3 file types
     """
     song = None
-    if file_type == 'oga':
-        song = AudioSegment.from_ogg(audio_file)
 
-    if song:
-        if song.duration_seconds <= length:
-            """ If the song is less than 30 seconds, give them half """
-            song = song[:song.duration_seconds / 2]
+    try:
+        file_name = audio_file_name.split(".")[0]
+        file_type = audio_file_name.split(".")[1]
 
-        else:
-            song = song[(length - (length / 2)):(length + (length * 2))]
+        if file_type == 'oga':
+            file_type = 'ogg'
+            song = AudioSegment.from_ogg(audio_file_name)
+        elif file_type == 'mp3':
+            song = AudioSegment.from_mp3(audio_file_name)
 
-        song.export(u"{}_slice.{}".format(audio_file, file_type), format=file_type)
+        if song:
+            if song.duration_seconds * 1000 <= length:
+                """ If the song is less than 30 seconds, give them half """
+                song = song[:(song.duration_seconds / 2) * 1000]
+
+            else:
+                lower_bound = length - (length / 2)
+                song = song[lower_bound:(length * 2) - lower_bound].fade_in(2000).fade_out(3000)
+
+            song.export(u"{}_slice.{}".format(file_name, file_type), format=file_type)
+
+    except IOError as e:
+        print e.errno
+        print e
+
 
 # convert_to_audio(soundfont='fluidr3_gm2-2.sf2', output_path='../pianosite/public/media/audio/')
