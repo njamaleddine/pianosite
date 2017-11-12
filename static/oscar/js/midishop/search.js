c4bb0094066fc7a74041bfebbed17bd34b2b4cb5
Nabil Jamaleddine <nabilj123@gmail.com>
2017-11-12 15:04:17 -0500

    feat: add arrows to search facet/filter panels & show on collapse

--- static/oscar/js/midishop/search.js
+++ static/oscar/js/midishop/search.js
@@ -1,14 +1,21 @@
 'use strict';
 $(document).ready(function(){
+  var panelArrowIcon = '.product-search-panel-arrow';
+
   function collapseSearchPanels() {
     if ($(window).width() < 768) {
+      $(panelArrowIcon).show();
       $('.search-panel-collapse').collapse('hide');
     } else {
+      $(panelArrowIcon).hide();
       $('.search-panel-collapse').collapse('show');
     }
   }
 
   $(window).on('resize', function(){
+    $('.search-panel-collapse').collapse({'toggle': true})
     collapseSearchPanels();
   });
+  $('.search-panel-collapse').collapse({'toggle': false})
+  collapseSearchPanels();
 });
